8ce3d785203daa554611d70d382f80ec7a652315
rkt@xps13 <highpear.0619@gmail.com>
2021-02-13 02:47:29 +0900

    18 strings

---
+++ 18_strings/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut my_str = String::from("my name is Riku.");
+
+    println!("{}", my_str);
+
+    // length -> my_str.len()
+
+    my_str.push_str(" Good bye !");
+    println!("{}", my_str);
+
+
+}
